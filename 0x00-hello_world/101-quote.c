26c7ad971f643851cb4b6f44616a2d7db1420bd6
Mark-Vincent-Otieno <mvmarkvo@gmail.com>
2023-02-16 18:28:21 -0800

    101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a C program that prints an exact line to the standatrd error.
+ * main - program that prints an exact line to the standatrd error.
  * Return: 1 will be (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
